f2fbf93985dacc6a112d6cd02cca0faa0f327812
Yaroslav <lynnyk265@gmail.com>
2024-04-06 14:01:57 +0300

    upd request methods

--- src/main/java/com/example/starylevbackendspring/controllers/HomeController.java
+++ src/main/java/com/example/starylevbackendspring/controllers/HomeController.java
@@ -1,20 +1,21 @@
 package com.example.starylevbackendspring.controllers;
 
-import com.example.starylevbackendspring.Book;
+import com.example.starylevbackendspring.helpers.JsonUtility;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:3000")
-public class BooksController {
-    @GetMapping(value = "/books")
-    public Book getMyItem(){
-        Book book = new Book();
-        book.setName("123");
-        return book;
+public class HomeController {
+    private static final String PATH = "static/data/home-images.json";
+    @GetMapping(value = "/home-images")
+    public ResponseEntity<String> getHomeImages() throws IOException {
+        return JsonUtility.getJson(PATH);
     }
 }
---
+++ src/main/java/com/example/starylevbackendspring/helpers/JsonUtility.java
@@ -0,0 +1,18 @@
+package com.example.starylevbackendspring.helpers;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+public class JsonUtility {
+    public static ResponseEntity<String> getJson(String path) throws IOException {
+        ClassPathResource resource = new ClassPathResource(path);
+        InputStream inputStream = resource.getInputStream();
+        String jsonString = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        return ResponseEntity.ok(jsonString);
+    }
+}
